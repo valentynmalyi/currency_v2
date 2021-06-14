4dd3cf80887dd0a43756caf197025f234a6e0db6
valentynmalyi <valentynmalyi@gmail.com>
2021-06-14 19:27:47 +0300

    add start day

--- currency/settings/history.py
+++ currency/settings/history.py
@@ -1,3 +1,11 @@
 from datetime import date
 
-START_DATE = date(year=2010, month=1, day=1)
+from environ import Env
+
+env = Env()
+
+start_date_year = env.int("START_DATE_YEAR")
+start_date_month = env.int("START_DATE_MONTH")
+start_date_day = env.int("START_DATE_DAY")
+
+START_DATE = date(year=start_date_year, month=start_date_month, day=start_date_day)
